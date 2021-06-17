aba7a61666bf3d58742c2164a2d271f2b51c83b7
Kizlo Taras <orleon.nilkford@gmail.com>
2021-06-17 21:34:48 +0300

    Update build.cmd

--- Infrastructure/build.cmd
+++ Infrastructure/build.cmd
@@ -1,1 +1,5 @@
+# load external docker images into the local repository
+docker pull mcr.microsoft.com/dotnet/core/sdk:3.1.300
+
+# build our own docker images and services
 docker-compose -f docker-compose.yml -f docker-compose.development.yml build
