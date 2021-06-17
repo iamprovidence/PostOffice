e36cd6e3249844470223709fb64a484692399a55
Kizlo Taras <orleon.nilkford@gmail.com>
2021-06-17 21:35:19 +0300

    Update build.cmd

--- Infrastructure/build.cmd
+++ Infrastructure/build.cmd
@@ -1,4 +1,5 @@
 # load external docker images into the local repository
+# to prevent their reload
 docker pull mcr.microsoft.com/dotnet/core/sdk:3.1.300
 
 # build our own docker images and services
